413eedaabaecdd395f90dceeaaf6474eca68319b
chenjing <13891739600@163.com>
2024-07-18 11:59:34 +0800

    Initial commit

---
+++ CJ_TAST_12/dataset_DGTNet.py
@@ -0,0 +1,211 @@
+from ast import parse
+import argparse
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import warnings
+
+from PyEMD import EMD,CEEMDAN
+import seaborn as sns
+import matplotlib.pyplot as plt
+# pip install EMD-signal
+
+
+def emd_imf(signal):
+    """
+    This function is to calculate EMD of the time series.
+    :params: signal: list
+
+    :return: res_dict: a dict consists of the different imf list value
+    """
+    if isinstance(signal, list):
+        signal = np.array(signal)
+    assert isinstance(signal, np.ndarray)
+    IMFs = EMD().emd(signal, np.arange(len(signal)))
+    # IMFs = CEEMDAN().emd(signal, np.arange(len(signal)))
+    res_dict = {}
+    for _ in range(IMFs.shape[0]):
+        res_dict[f'imf_{_}'] = IMFs[_].tolist()
+    return res_dict
+
+
+def calculate_imf_features(n_zones, index_pair_for_one, zones_dict:dict, ticker_ASD_dict:dict, n_imf_use=5) -> np.ndarray:
+    """
+    compute the EMD.
+
+    :return: imf_features
+    """
+    assert isinstance(n_imf_use, int)
+    imf_features = np.zeros((n_zones, n_zones, n_imf_use))
+    ticker_A, ticker_B = None, None
+    for pair in index_pair_for_one:
+
+        if ticker_A != zones_dict[pair[0]]:
+            ticker_A = zones_dict[pair[0]]
+            ticker_A_ASD = ticker_ASD_dict[ticker_A]
+        if ticker_B != zones_dict[pair[1]]:
+            ticker_B = zones_dict[pair[1]]
+            ticker_B_ASD = ticker_ASD_dict[ticker_B]
+        
+        ef = [0] * n_imf_use
+        for n_imf in list(range(1, n_imf_use+1)):  # n_imf_to_exact = n_imf_use
+            if f'imf_{n_imf}' in ticker_A_ASD and f'imf_{n_imf}' in ticker_B_ASD:
+                # to get both imf for both 2 tickers
+                ef[n_imf-1] = (np.corrcoef(ticker_A_ASD[f'imf_{n_imf}'],
+                                            ticker_B_ASD[f'imf_{n_imf}'])[0][1]
+                                )
+            else:
+                break
+        imf_features[pair[0]][pair[1]], imf_features[pair[1]][pair[0]] = np.array(ef), np.array(ef)
+    
+    return imf_features
+
+
+def process_data(df, n_lookback_days, n_lookforward_days, adj_mat_method='fully_connected', use_tqdm=True, **kwargs):
+    """
+    This is the main part of generate graph.
+    """
+    if n_lookforward_days > n_lookback_days:
+        warnings.warn(f'The number of lookforward days ({n_lookforward_days}) is lager than lookback days ({n_lookback_days}). Please conside using longer lookback days')
+    _graph_data, _graph_label = [], []
+    zones = df.columns.to_list()   # 每一列的列名存在列表里
+    n_zones, zones_dict = len(zones), dict(zip(range(len(zones)), zones))     # 228*228矩阵，zones_dict分别用0123456对应列名
+    ranges = range(len(df) - n_lookforward_days - n_lookback_days)   # 包含0，range（0, 3787）
+    print(f'tqdm used: {use_tqdm}')
+    range_iterable = tqdm(ranges) if use_tqdm else ranges
+    for adate in range_iterable:    # 挨个取样本
+        if not use_tqdm:
+            print(f'Generating graph for time {df.index[adate+n_lookback_days]} ...... ', end='')
+        
+        lookback_period_df = df[adate:adate+n_lookback_days]
+        lookforward_period_df = df[adate+n_lookback_days:adate+n_lookback_days+n_lookforward_days]
+    
+
+        node_features = lookback_period_df.to_numpy().transpose()
+        
+        # node_features (normalization)
+        nf_max, nf_min = np.amax(node_features, axis=1), np.amin(node_features, axis=1)    # 在axis=1维度上进行节点特征的最小值最大值计算
+
+        if np.any(nf_max == nf_min):
+            nf_max[np.where(nf_max - nf_min == 0)], nf_min[np.where(nf_max - nf_min == 0)] = 1, 0
+        
+        nf_MIN = np.repeat(nf_min, n_lookback_days, axis=0).reshape(n_zones, n_lookback_days)
+        nf_MAX = np.repeat(nf_max, n_lookback_days, axis=0).reshape(n_zones, n_lookback_days)
+        node_features_normalization = (node_features-nf_MIN)/(nf_MAX - nf_MIN)
+        
+        # adj_matrix (fully connected or corr)
+        if adj_mat_method == 'fully_connected':
+            # all 1 except the diagonal
+            adj_mat = np.ones((n_zones, n_zones)) - np.eye(n_zones)
+        elif adj_mat_method == 'correlation':      # 皮尔逊相关
+            # based on correlation
+            correlation_matrix = np.abs(np.corrcoef(lookback_period_df.values, rowvar=False))
+            correlation_matrix = np.where(correlation_matrix == 1., 0, correlation_matrix)
+            correlation_matrix = np.where(correlation_matrix >= 0.8, 1, 0)
+            adj_mat = correlation_matrix
+        elif adj_mat_method == 'zero_mat':
+            # zero matrix
+            adj_mat = np.zeros((n_zones, n_zones))
+        elif adj_mat_method == 'random':
+            # random
+            b = np.random.random_integers(0, 1, size=(n_zones, n_zones))
+            adj_mat = b * b.T
+        else:
+            raise TypeError(f'Unsupported adj_matrix method: {adj_mat_method}!')
+        
+        ## calculate_imf_features
+        # 在adj的上三角找索引对
+        index_pair_for_one = np.argwhere(np.triu(adj_mat) == 1)
+        ticker_ASD_dict = dict.fromkeys(zones)
+        involde_index_idxs_np = np.unique(index_pair_for_one.flatten())
+        for index_idx in involde_index_idxs_np:
+            ticker = zones_dict[index_idx]
+            ticker_ASD_dict[ticker] = emd_imf(lookback_period_df[ticker].to_list())
+
+        #   7个节点 ，每个特征有个imfs，都存在ticker_ASASD_dict
+        imf_features = calculate_imf_features(n_zones, index_pair_for_one, zones_dict, ticker_ASD_dict, n_imf_use=5)
+
+        # 将IMF特征矩阵以对应的键名存储在imf_matries_dict字典中  5个7*7矩阵
+        imf_matries_dict = {
+            'imf_1_matix':imf_features[:,:,0],
+            'imf_2_matix':imf_features[:,:,1],
+            'imf_3_matix':imf_features[:,:,2],
+            'imf_4_matix':imf_features[:,:,3],
+            'imf_5_matix':imf_features[:,:,4],
+        }
+        
+        # label
+        label = lookforward_period_df.to_numpy().transpose()
+
+        # label (normalization)
+        label_MIN = np.repeat(nf_min, n_lookforward_days, axis=0).reshape(n_zones, n_lookforward_days)
+        label_MAX = np.repeat(nf_max, n_lookforward_days, axis=0).reshape(n_zones, n_lookforward_days)
+        label_normalization = (label-label_MIN)/(label_MAX - label_MIN)
+        label_normalization = label_normalization.flatten()
+        
+        name_of_sample = f"ETTm1_LookbackInculde[{lookback_period_df.index[0]} to {lookback_period_df.index[-1]}]_Predict[{lookforward_period_df.index[0]} to {lookforward_period_df.index[-1]}]"
+        
+        
+        G = {
+            'date': name_of_sample,
+            'node_feat': node_features_normalization,
+            'imf_matries_dict': imf_matries_dict,   # 228*228*5
+            'adj_mat': adj_mat,                   # 皮尔逊系数算的 228*228
+            'node_local_MAX': nf_max,
+            'node_local_MIN': nf_min,
+            'node_global_MAX': None,
+            'node_global_MIN': None
+        }
+        _graph_data.append(G)
+        _graph_label.append(list(label_normalization))    # 48*7的矩阵转为list 336的list 即只预测336个值即可
+        if not use_tqdm:
+            print('Done !')
+            
+    return _graph_data, _graph_label
+
+
+def list_split(L, n_split):
+    """
+    Split the list
+    """
+    assert isinstance(L, list)
+    k, m = divmod(len(L), n_split)
+    _ = (L[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n_split))
+    return list(_)
+
+
+def generate_data(n_lookback_days:int, n_lookforward_days:int, adj_mat_method:str, use_tqdm:bool):
+    import time
+    start_time = time.time()
+    # df = pd.read_csv(f'Data/ETTm1_{n_lookforward_days}/source/ETTm1.csv', index_col='date')   # 69680*7
+    df = pd.read_csv(f'Data/PEMS/PEMS_BAY.csv', header=None)
+    assert len(df) != 0                     # check the input df, it should noe be None.
+    # df = df[:200]
+    # df = df[:5000]   # 16700
+    # fill填充
+    df.ffill(inplace=True)      # 前向填充
+    df.bfill(inplace=True)
+    graph_data, graph_label = process_data(df, n_lookback_days, n_lookforward_days, adj_mat_method=adj_mat_method, use_tqdm=use_tqdm)    # correlation
+    assert len(graph_data) == len(graph_label)  # the length of data_mol and label should be same
+    assert graph_data[0] is not None      # Check the datamol should be non-empty
+    end_time = time.time()
+    print(f'Generate ETTm1 data complete! Total time: {end_time-start_time}')
+    return graph_data, graph_label
+
+# ======================================================================================
+
+
+if __name__ == '__main__':
+    # init args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lb", type=int, help="n_lookback_days", default=12)
+    parser.add_argument("--lf", type=int, help="n_lookforward_days", default=12)
+    parser.add_argument("--adj_mat_method", type=str, help="adj_mat_method", default='zero_mat', choices=['zero_mat', 'fully_connected', 'correlation', 'random'])
+    parser.add_argument("--use_tqdm", type=str, help="use tqdm or not", default='True', choices=['True', 'False'])
+    args = parser.parse_args()
+    assert isinstance(args.lb, int)
+    assert isinstance(args.lf, int)
+    # generate ETTm1_48 datas
+    graph_data, graph_label = generate_data(args.lb, args.lf, args.adj_mat_method, bool(args.use_tqdm))
+
+
---
+++ CJ_TAST_12/readme.md
@@ -0,0 +1,48 @@
+# DGTNet:Dynamic Graph Attention Transformer Network for Traffic Flow Forecasting 
+![Python 3.8](https://img.shields.io/badge/Python-3.8-green.svg?style=plastic)
+![PyTorch 1.8](https://img.shields.io/badge/PyTorch%20-%23EE4C2C.svg?style=plastic)
+
+<p align='center'>
+  <img src="DGTNet.png" alt="architecture" width="600"/>
+</p>
+
+
+## Code
+- `main.py` : main DGTNet model interface with training and testing
+- `DGT.py` : with DGTNet class implementation
+- `utils.py` : utility functions and dataset parameter
+- `dataset_DGTNet.py` : generate graph from dataset
+
+### Dataset
+The traffic data files for Los Angeles (METR-LA) and the Bay Area (PEMS-BAY), i.e., `metr-la.h5` and `pems-bay.h5`, are available at [Google Drive](https://drive.google.com/open?id=10FOTa6HXPqX8Pf5WRoRwcFnW9BrNZEIX) or [Baidu Yun](https://pan.baidu.com/s/14Yy9isAIZYdU__OYEQGa_g), and should be
+put into the `data/` folder.
+
+
+## Model parameters
+The parameters setting can be found in `utils.py`.
+- `l_backcast` : lengths of backcast
+- `d_edge` : number of IMF used
+- `d_model` : the time embedding dimension
+- `N` : number of Self_Attention_Block
+- `h` : number of head in Multi-head-attention
+- `N_dense` : number of linear layer in Sequential feed forward layers
+- `n_output` : number of output (lengths of forecast $\times$ number of node)
+- `n_nodes` : number of node (aka number of time series)
+- `lambda` : the initial value of the trainable lambda $\alpha_i$
+- `dense_output_nonlinearity` the nonlinearity function in dense output layer
+
+## Requirements
+- Python 3.8
+- PyTorch = 1.8.0 (with corresponding CUDA version)
+- Pandas = 1.4.0
+- Numpy = 1.22.2
+- PyEMD = 1.2.1
+
+Dependency can be installed using the following command:
+```bash
+pip install -r requirements.txt
+```
+
+
+## Contact
+If you have any questions, please feel free to contact Chen jing (Email: 13891739600@163.com).
